2b79a8488ad7675870d4f9fe66af7c5cbd8e4915
javerthu <867216905@qq.com>
2019-10-18 21:10:00 +0800

    jupyter 笔记

--- 14-Spider/v26.py
+++ 14-Spider/v26.py
@@ -1,5 +1,6 @@
 '''
 findall案例
+加贪婪匹配的机制
 '''
 import re
 
@@ -10,6 +11,19 @@ s = pattern.findall("i am 18 years odl and 185 high")
 print(s)
 
 s = pattern.finditer("i am 18 years odl and 185 high")
+
+print(type(s))
+
+for i in s:
+    print(i.group())
+
+p = re.compile(r'.*?w')
+
+s = p.findall("awbwccwdwvw123w")
+
+print(s)
+
+s = p.finditer("awbwccwdwvw123w")
 
 print(type(s))
 
